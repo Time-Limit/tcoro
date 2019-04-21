b7ee6dd4db371adac73199aebf0ee286ee5bfc0e
tlimit <tlimit@outlook.com>
2019-04-21 16:49:36 +0800

    删除无用文件

--- as/returnValue.s
+++ as/returnValue.s
@@ -5,6 +5,15 @@
 .global _start
 
 _start:
+    subl $4, %esp
+    pushl %esp #将 esp 的值压入栈，即将申请的栈内存地址压入栈，作为参数传递给 work
+    call work
     movl $1, %eax
-    movl $45, %ebx
+    movl 4(%esp), %ebx
     int $0x80
+
+.type work, @function
+work:
+    movl 4(%esp), %eax #将内存地址取出，赋给eax
+    movl $10, (%eax) #将 10 写入到 %eax + 0 处的内存
+    ret

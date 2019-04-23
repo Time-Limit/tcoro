ff3e630bf337a62d97c5aaa428dd7cf33d8dc70d
tlimit <tlimit@outlook.com>
2019-04-23 18:23:58 +0800

    堆栈打印实验

--- as/clean.sh
+++ as/clean.sh
@@ -4,7 +4,7 @@ IFS="
 "
 
 for line in `ls -al ./ | grep "^-"`;do
-    fileName=`echo ${line} | awk '{print $NF;}' | grep "\.\(sh\|s\)$" -v | grep "^\." -v`;
+    fileName=`echo ${line} | awk '{print $NF;}' | grep "\.\(sh\|s\|cpp\|h\)$" -v | grep Makefile -v | grep "^\." -v`;
     if [ "x"${fileName} != "x" ];then
         echo ${fileName}
         rm ${fileName}
